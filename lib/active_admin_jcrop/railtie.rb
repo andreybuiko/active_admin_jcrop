5c11f04f71f11054bea25a96f00db7600443811f
andrey <buikoandrey@gmail.com>
2017-10-13 12:11:24 +0300

    dd scsss, remove 'registers' methods

--- lib/active_admin_jcrop/railtie.rb
+++ lib/active_admin_jcrop/railtie.rb
@@ -2,21 +2,21 @@ module ActiveAdminJcrop
   class Railtie < Rails::Railtie
     initializer "active_admin_jcrop_railtie.configure_rails_initialization" do
       ActiveAdmin::DSL.send(:include, ActiveAdminJcrop::DSL)
-      ActiveAdmin.setup do |config|
-        [ 'active_admin_jcrop/crop_modal',
-          "active_admin_jcrop/jquery.color",
-          "active_admin_jcrop/jquery.Jcrop.min"
-        ].each do |js|
-          config.register_javascript js
-        end
-
-        [ "active_admin_jcrop/crop_modal",
-          'active_admin_jcrop/jquery.Jcrop.min'
-        ].each do |css|
-          config.register_stylesheet css
-        end
-
-      end
+      # ActiveAdmin.setup do |config|
+      #   [ 'active_admin_jcrop/crop_modal',
+      #     "active_admin_jcrop/jquery.color",
+      #     "active_admin_jcrop/jquery.Jcrop.min"
+      #   ].each do |js|
+      #     config.register_javascript js
+      #   end
+      #
+      #   [ "active_admin_jcrop/crop_modal",
+      #     'active_admin_jcrop/jquery.Jcrop.min'
+      #   ].each do |css|
+      #     config.register_stylesheet css
+      #   end
+      #
+      # end
     end
     initializer "active_admin_jcrop_railtie.view_helpers" do
       ActionView::Base.send :include, ViewHelpers
